9e6e2c6c51947b7c12356ac2f33d4be7c13f92d0
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-05-04 21:26:14 -0700

    'Auth context'

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,52 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import AuthContext from "./authContext";
+import authReducer from "./authReducer";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from "../types";
+
+const AuthState = ({ children }) => {
+  const intialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    error: null,
+    user: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, intialState);
+
+  // Load user
+
+  // Register user
+
+  // Login user
+
+  // Logout user
+
+  // Clear errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        error: state.error,
+        user: state.user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
